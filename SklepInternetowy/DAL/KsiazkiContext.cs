ac43f5b9e655b5fa4ee6a5393a565d90bc0e33cd
Patryk Adamek <ptk.adamek@gmail.com>
2017-07-12 22:59:19 +0200

    Dodanie ConnectionString w pliku Web.config

---
+++ SklepInternetowy/DAL/KsiazkiContext.cs
@@ -0,0 +1,17 @@
+﻿using SklepInternetowy.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SklepInternetowy.DAL
+{
+    public class KsiazkiContext : DbContext
+    {
+        public DbSet<Ksiazka> Ksiazki { get; set; }
+        public DbSet<Kategoria> Kategorie { get; set; }
+        public DbSet<Zamowienie> Zamowienia { get; set; }
+        public DbSet<PozycjaZamowienia> PozycjeZamowienia { get; set; }
+    }
+}
