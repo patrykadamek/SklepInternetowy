96f320c65ec5587fc4158e81fcc559f0d3a0a273
Patryk Adamek <ptk.adamek@gmail.com>
2017-07-12 23:27:19 +0200

    Pierwszy test bazy danych

--- SklepInternetowy/Controllers/HomeController.cs
+++ SklepInternetowy/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SklepInternetowy.DAL;
+using SklepInternetowy.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,14 @@ namespace SklepInternetowy.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        private KsiazkiContext db = new KsiazkiContext();
+
         public ActionResult Index()
         {
+            Kategoria kategoria = new Kategoria { NazwaKategorii = "asp.net mvc", NazwaPlikuIkony = "aspNetMvc.png", OpisKategorii = "opis" };
+            db.Kategorie.Add(kategoria);
+            db.SaveChanges();
+
             return View();
         }
     }
