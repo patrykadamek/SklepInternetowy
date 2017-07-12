c89c6329bce0a1015042352cd4a008a25c63139a
Patryk Adamek <ptk.adamek@gmail.com>
2017-07-12 23:06:03 +0200

    Dodanie KsiazkiContext oraz wyłączenie konwencji dodającej do nazw tabel końcówkę "s"

--- SklepInternetowy/DAL/KsiazkiContext.cs
+++ SklepInternetowy/DAL/KsiazkiContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
@@ -9,9 +10,21 @@ namespace SklepInternetowy.DAL
 {
     public class KsiazkiContext : DbContext
     {
+        public KsiazkiContext() : base("KsiazkiContext")
+        {
+
+        }
         public DbSet<Ksiazka> Ksiazki { get; set; }
         public DbSet<Kategoria> Kategorie { get; set; }
         public DbSet<Zamowienie> Zamowienia { get; set; }
         public DbSet<PozycjaZamowienia> PozycjeZamowienia { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Wyłącza konwencję, która dodaje automatycznie do nazw tabel końcówkę "s" tworząc liczbę mnogą
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
 }
