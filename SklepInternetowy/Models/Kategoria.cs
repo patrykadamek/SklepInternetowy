3961cc2522451db80e3bef4406e5ddca9368a7cc
Patryk Adamek <ptk.adamek@gmail.com>
2017-07-12 22:43:33 +0200

    Dodanie Atrybutów do klas Ksiazka, Kategoria, Zamowienie

--- SklepInternetowy/Models/Kategoria.cs
+++ SklepInternetowy/Models/Kategoria.cs
@@ -1,21 +1,19 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SklepInternetowy.Models
 {
     public class Kategoria
     {
         public int KategoriaId { get; set; }
+        [Required(ErrorMessage = "Wprowadź nazwę kategorii")]
+        [StringLength(100)]
         public string NazwaKategorii { get; set; }
+        [Required(ErrorMessage = "Wprowadź opis kategorii")]
         public string OpisKategorii { get; set; }
         public string NazwaPlikuIkony { get; set; }
 
         public virtual ICollection<Ksiazka> Ksiazki { get; set; }
-
-
-
-
-
-
 
     }
 }
--- SklepInternetowy/Models/Ksiazka.cs
+++ SklepInternetowy/Models/Ksiazka.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,9 +10,14 @@ namespace SklepInternetowy.Models
     {
         public int KsiazkaId { get; set; }
         public int KategoriaId { get; set; }
+        [Required(ErrorMessage = "Wprowadź nazwę książki")]
+        [StringLength(100)]
         public String TytulKsiazki { get; set; }
+        [Required(ErrorMessage = "Wprowadź nazwę autora")]
+        [StringLength(100)]
         public String AutorKsiazki { get; set; }
         public DateTime DataWydania { get; set; }
+        [StringLength(100)]
         public String NazwaPlikuObrazka { get; set; }
         public String OpisKsiazki { get; set; }
         public decimal CenaKsiazki { get; set; }
