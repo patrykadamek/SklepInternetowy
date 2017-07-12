18e36297ac37bf192056411a46d818f8c488a81c
Patryk Adamek <ptk.adamek@gmail.com>
2017-07-12 22:31:14 +0200

    Dodanie klas modelu - Ksiazka, Kategoria, Zamowienie, PozycjaZamowienia

---
+++ SklepInternetowy/Models/Kategoria.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace SklepInternetowy.Models
+{
+    public class Kategoria
+    {
+        public int KategoriaId { get; set; }
+        public string NazwaKategorii { get; set; }
+        public string OpisKategorii { get; set; }
+        public string NazwaPlikuIkony { get; set; }
+
+        public virtual ICollection<Ksiazka> Ksiazki { get; set; }
+
+
+
+
+
+
+
+    }
+}
---
+++ SklepInternetowy/Models/Ksiazka.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SklepInternetowy.Models
+{
+    public class Ksiazka
+    {
+        public int KsiazkaId { get; set; }
+        public int KategoriaId { get; set; }
+        public String TytulKsiazki { get; set; }
+        public String AutorKsiazki { get; set; }
+        public DateTime DataWydania { get; set; }
+        public String NazwaPlikuObrazka { get; set; }
+        public String OpisKsiazki { get; set; }
+        public decimal CenaKsiazki { get; set; }
+        public bool Bestseller { get; set; }
+        public bool Ukryty { get; set; }
+        public virtual Kategoria Kategoria { get; set; }
+    }
+}
